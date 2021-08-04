107df820031c8d61a56c4caf7d2effc399e449fe
minhmoc710 <minhmoc710@gmail.com>
2021-08-04 16:39:17 +0700

    add join with multiple accounts feature

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
-python join_groups.py -f "group_ids.txt" -o "joined_group_ids.txt"
-
-usage: join_groups.py [-h] [-f FILE] [-o OUTFILE]
+## example:
+```
+python join_groups.py -a "fb_accounts.csv" -o "out.json" -f "group_ids.txt"
+```
+## usage: 
+```join_groups.py [-h] [-f FILE] [-o OUTFILE] [-a ACCOUNTFILE]
 
 Join facebook groups with ids specified in a file
 
 optional arguments:
-
-
   -h, --help            show this help message and exit
-
-  
-  -f FILE, --file FILE  path to file which contains group ids
-
-
+  -f FILE, --file FILE  path to text file which contains group ids that we need to join
   -o OUTFILE, --outfile OUTFILE
-                        path to file which contains group ids
+                        path to json file which contains joined group ids
+  -a ACCOUNTFILE, --accountfile ACCOUNTFILE
+                        path to csv file which contains facebook accounts```
--- join_groups.py
+++ join_groups.py
@@ -1,13 +1,16 @@
 from os import close
-from selenium import webdriver
 from time import sleep
+import pandas as pd
+import json
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.options import Options 
 import argparse
 
 parser = argparse.ArgumentParser(description="Join facebook groups with ids specified in a file")
-parser.add_argument('-f','--file',help = "path to file which contains group ids")
-parser.add_argument('-o','--outfile',help = "path to file which contains group ids")
+parser.add_argument('-f','--file',help = "path to text file which contains group ids that we need to join")
+parser.add_argument('-o','--outfile',help = "path to json file which contains joined group ids")
+parser.add_argument('-a','--accountfile',help = "path to csv file which contains facebook accounts")
 args = parser.parse_args()
 
 def login_fb(driver, usr, pwd):
@@ -22,6 +25,7 @@ def login_fb(driver, usr, pwd):
 
     login_box = driver.find_element_by_css_selector('button[name="login')
     login_box.click()
+    print("Logged in")
 
 def request_join_group(driver, group_id):
     base_url = "https://m.facebook.com/groups/"
@@ -36,13 +40,15 @@ def request_join_group(driver, group_id):
         print(e)
     sleep(1)
 
-def request_join_from_file(driver, group_ids_file):
+def request_join_from_file(driver, group_ids_file, already_joined_groups = []):
     with open(group_ids_file, "r") as f:
         group_ids = [line[:-1] for line in f.readlines()]
     for group_id in group_ids:
+        if group_id in already_joined_groups:
+            continue
         request_join_group(driver, group_id)
 
-def joined_group(driver, group_id):
+def check_joined_group(driver, group_id):
     """Check if joined group or not"""
 
     base_url = "https://m.facebook.com/groups/"
@@ -54,42 +60,74 @@ def joined_group(driver, group_id):
     except:
         return False
 
-def check_joined_from_file(driver, group_ids_file, joined_groups_file):
+def check_joined_from_file(driver, group_ids_file):
     print("Checking if joined groups...")
     with open(group_ids_file, "r") as f:
         group_ids = [line.strip() for line in f.readlines()]
     unjoined_groups = []
-    joined_groups = set()
+    joined_groups = []
     for group_id in group_ids:
-        if joined_group(driver, group_id):
-            joined_groups.add(group_id)
+        if check_joined_group(driver, group_id):
+            joined_groups.append(group_id)
             print(f"Joined {group_id}")
         else:
             print(f"Didn't join {group_id}")
             unjoined_groups.append(group_id)
-    
-    with open(group_ids_file, "w") as f:
+    return unjoined_groups, joined_groups
+def write_unjoined_groups(unjoined_groups, group_ids_file):
+    with open(group_ids_file, 'w') as f:
         for group_id in unjoined_groups:
-            f.write(group_id + "\n")
-    try: 
-        with open(joined_groups_file, "r") as f:
-            for line in f.readlines():
-                joined_groups.add(line)
-    except:
-        "File didn't exists. Creating new file"
-    with open(joined_groups_file, "w") as f:
-        for group_id in joined_groups:
             f.write(str(group_id) + "\n")
+def write_joined_groups(joined_groups, joined_groups_file, usr, pwd):
+    groups_info = {}
+    account = {usr: pwd}
+    try:
+        with open(joined_groups_file, 'r') as f:
+            groups_info =json.load(f)
+    except:
+        "File doesn't exist. Creating new file"
+    for group_id in joined_groups:
+        print(group_id)
+        print(groups_info.keys)
+        if not group_id in groups_info.keys():
+            groups_info[group_id] = [account]
+        else:
+            if not account in groups_info[group_id]:
+                groups_info[group_id].append(account)
+    print(type(groups_info))
+    with open(joined_groups_file, 'w') as f:
+        json.dump(groups_info, f)
+def get_already_joined_groups(joined_groups_file, usr, pwd):
+    account = {usr:pwd}
+    joined_groups_info = {}
+    joined_groups = []
+    try:
+        with open(joined_groups_file, 'r') as f:
+            joined_groups_info = json.load(f)
+    except:
+        return []
 
-if __name__ == "__main__":
-    DRIVER_PATH = r"drivers/geckodriver"
+    for group_id in joined_groups_info:
+        if account in joined_groups_info[group_id]:
+            joined_groups.append(group_id)   
+    return joined_groups
+def join_multiple_accounts(group_ids_file, joined_groups_file, fb_accounts_file):
     options = Options()
     options.headless = True
-    driver = webdriver.Firefox(executable_path = DRIVER_PATH, options = options)
+    DRIVER_PATH = r"drivers/geckodriver.exe"
+    accounts_df = pd.read_csv(fb_accounts_file)
+    for _, row in accounts_df.iterrows():
+        driver = webdriver.Firefox(executable_path = DRIVER_PATH, options = options)
+        login_fb(driver, row['user'], row['pw'])
 
-    usr = "522160734"
-    pwd = "matkhau2213"
-    login_fb(driver, usr, pwd)
-    request_join_from_file(driver, args.file)
-    check_joined_from_file(driver, args.file, args.outfile)
-    driver.close()
+        joined_groups = get_already_joined_groups(joined_groups_file, row['user'], row['pw'])
+        request_join_from_file(driver, group_ids_file, joined_groups)
+        unjoined, joined = check_joined_from_file(driver, group_ids_file)
+
+        # write_unjoined_groups(unjoined, group_ids_file)
+        write_joined_groups(joined, joined_groups_file, row['user'], row['pw'])
+        driver.close()
+if __name__ == "__main__":
+    
+    join_multiple_accounts(fb_accounts_file= args.accountfile,joined_groups_file= args.outfile, group_ids_file=args.file)
+    # print("do nothing")
