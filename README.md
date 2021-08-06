a3e03c1cfeba3e959caefaefb197d5b30678ba03
minhmoc710 <minhmoc710@gmail.com>
2021-08-06 14:55:05 +0700

    change input and output file format

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 ## example:
 ```
-python join_groups.py -a "fb_accounts.csv" -o "out.json" -f "group_ids.txt"
+python join_groups.py -a "fb_accounts.csv"  -f "group_ids.csv"
 ```
 ## usage: 
-```join_groups.py [-h] [-f FILE] [-o OUTFILE] [-a ACCOUNTFILE]
+```usage: join_groups.py [-h] [-f FILE] [-a ACCOUNTFILE]
 
 Join facebook groups with ids specified in a file
 
 optional arguments:
   -h, --help            show this help message and exit
   -f FILE, --file FILE  path to text file which contains group ids that we need to join
-  -o OUTFILE, --outfile OUTFILE
-                        path to json file which contains joined group ids
   -a ACCOUNTFILE, --accountfile ACCOUNTFILE
                         path to csv file which contains facebook accounts```
--- join_groups.py
+++ join_groups.py
@@ -9,14 +9,16 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Join facebook groups with ids specified in a file")
 parser.add_argument('-f','--file',help = "path to text file which contains group ids that we need to join")
-parser.add_argument('-o','--outfile',help = "path to json file which contains joined group ids")
 parser.add_argument('-a','--accountfile',help = "path to csv file which contains facebook accounts")
 args = parser.parse_args()
 
 def login_fb(driver, usr, pwd):
+    print("_"*60)
     print("Logging in")
     driver.get("https://www.facebook.com/")
 
+    print(usr)
+    print(pwd)
     username_box = driver.find_element_by_id('email')
     username_box.send_keys(usr)
 
@@ -25,34 +27,49 @@ def login_fb(driver, usr, pwd):
 
     login_box = driver.find_element_by_css_selector('button[name="login')
     login_box.click()
-    print("Logged in")
+    sleep(1)
+    if driver.current_url == r"https://www.facebook.com/":
+        print(f"Logged in successfully with {usr}")
+        return True
+    else: 
+        print(f"Log in failed! Please check user name {usr}")
+        return False
 
 def request_join_group(driver, group_id):
     base_url = "https://m.facebook.com/groups/"
 
-    driver.get(base_url + group_id)
+    driver.get(base_url + str(group_id))
     sleep(1)
     try:
         join_box = driver.find_element_by_css_selector("button[label='Tham gia nhóm']")
         join_box.click()
+        print(f"Requested to join group {group_id}")
     except Exception as e:
-        print("Can't join group because of exception: ")
-        print(e)
+        try:
+            cancel_request_box = driver.find_element_by_css_selector('button[label = "Hủy yêu cầu"]')
+            if cancel_request_box != None:
+                print(f"Already requested {group_id}, waiting to be approved!") 
+        except:
+            x = driver.find_element_by_tag_name('body').text
+            if x.find("Không thể xử lý yêu cầu của bạn") >= 0 :
+                print(f"Group doesn't exists. Please check group id {group_id}")
+            else:
+                print("Can't join group because of exception: ")
+                print(e)
     sleep(1)
 
-def request_join_from_file(driver, group_ids_file, already_joined_groups = []):
-    with open(group_ids_file, "r") as f:
-        group_ids = [line[:-1] for line in f.readlines()]
-    for group_id in group_ids:
-        if group_id in already_joined_groups:
-            continue
-        request_join_group(driver, group_id)
-
+def request_join_from_csv(driver, groups_file, usr):
+    groups_df = pd.read_csv(groups_file, na_values="")
+    for _, row in groups_df.iterrows():
+        if usr in str(row['joined_accounts']).split(','):
+            print(f"Aready joined group {row['group_id']}")
+            return
+        request_join_group(driver, row['group_id'])        
 def check_joined_group(driver, group_id):
     """Check if joined group or not"""
 
     base_url = "https://m.facebook.com/groups/"
-    driver.get(base_url + group_id)
+    driver.get(base_url + str(group_id))
     sleep(1)
     try:
         _ = driver.find_element_by_css_selector("a[aria-label='Đã tham gia']")
@@ -60,20 +77,26 @@ def check_joined_group(driver, group_id):
     except:
         return False
 
-def check_joined_from_file(driver, group_ids_file):
+def check_joined_from_csv(driver, groups_file, usr):
     print("Checking if joined groups...")
-    with open(group_ids_file, "r") as f:
-        group_ids = [line.strip() for line in f.readlines()]
-    unjoined_groups = []
-    joined_groups = []
-    for group_id in group_ids:
+    groups_df = pd.read_csv(groups_file)
+    groups_df['joined_accounts'] = groups_df['joined_accounts'].astype(str)
+
+    for index, row in groups_df.iterrows():
+        joined_accounts = set(str(row['joined_accounts']).replace('nan', '').split(','))
+        if usr in joined_accounts:
+            print(f"Aready joined group {row['group_id']}")
+            continue
+        group_id = row['group_id']
         if check_joined_group(driver, group_id):
-            joined_groups.append(group_id)
+            joined_accounts.add(usr)
+            if not row['joined']:
+                groups_df.at[index, 'joined'] = True
             print(f"Joined {group_id}")
         else:
             print(f"Didn't join {group_id}")
-            unjoined_groups.append(group_id)
-    return unjoined_groups, joined_groups
+        groups_df.at[index, 'joined_accounts'] = ','.join([account for account in joined_accounts])
+    groups_df.to_csv(groups_file, index = False)
 def write_unjoined_groups(unjoined_groups, group_ids_file):
     with open(group_ids_file, 'w') as f:
         for group_id in unjoined_groups:
@@ -111,23 +134,26 @@ def get_already_joined_groups(joined_groups_file, usr, pwd):
         if account in joined_groups_info[group_id]:
             joined_groups.append(group_id)   
     return joined_groups
-def join_multiple_accounts(group_ids_file, joined_groups_file, fb_accounts_file):
+def join_multiple_accounts(groups_file, fb_accounts_file):
     options = Options()
     options.headless = True
     DRIVER_PATH = r"drivers/geckodriver"
     accounts_df = pd.read_csv(fb_accounts_file)
     for _, row in accounts_df.iterrows():
+        usr, pwd = row['user'].strip(), row['pw'].strip()
         driver = webdriver.Firefox(executable_path = DRIVER_PATH, options = options)
-        login_fb(driver, row['user'], row['pw'])
-
-        joined_groups = get_already_joined_groups(joined_groups_file, row['user'], row['pw'])
-        request_join_from_file(driver, group_ids_file, joined_groups)
-        unjoined, joined = check_joined_from_file(driver, group_ids_file)
-
-        # write_unjoined_groups(unjoined, group_ids_file)
-        write_joined_groups(joined, joined_groups_file, row['user'], row['pw'])
+        logged_in = login_fb(driver, usr, pwd)
+        if logged_in:
+            request_join_from_csv(driver, groups_file, usr)
+            check_joined_from_csv(driver, groups_file, usr)
         driver.close()
 if __name__ == "__main__":
-    
-    join_multiple_accounts(fb_accounts_file= args.accountfile,joined_groups_file= args.outfile, group_ids_file=args.file)
+    # options = Options()
+    # options.headless = False
+    # DRIVER_PATH = r"drivers/geckodriver.exe"
+    # driver = webdriver.Firefox(executable_path = DRIVER_PATH, options = options)
+    # logged_in = login_fb(driver, "minh.moc.2000", "dataemgomeer")
+    # request_join_from_csv(driver, "test_groups.csv", "minh.moc.2000")
+    # driver.close()
+    join_multiple_accounts(fb_accounts_file= args.accountfile, groups_file=args.file)
     # print("do nothing")
